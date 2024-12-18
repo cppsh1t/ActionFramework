54a3e2b1be396e883227fa85616a1b29139f7fdc
cppsh1t <1730648559@qq.com>
2024-12-18 18:26:30 +0800

    feat: use @antv/x6

--- front/BehaviourTreeBuilder/src/components/GraphView.vue
+++ front/BehaviourTreeBuilder/src/components/GraphView.vue
@@ -1,1 +1,20 @@
-<template></template>
+<template>
+  <div ref="graphContainer"></div>
+</template>
+<script setup lang="ts">
+import { Graph } from "@antv/x6"
+import { onMounted, ref } from "vue"
+
+const graphContainer = ref()
+
+onMounted(() => {
+  const graph = new Graph({
+    container: graphContainer.value,
+    width: 1366,
+    height: 768,
+    background: {
+      color: "#F2F7FA",
+    },
+  })
+})
+</script>
